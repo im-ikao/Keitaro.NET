45fc36573cfa9cfb4abab7086b566dcad39ef30c
Alexey Kostrykin <me@ikao.dev>
2022-08-17 19:44:21 +0300

    fix: campaign comments

--- Keitaro.NET/Clients/ICampaignsClient.cs
+++ Keitaro.NET/Clients/ICampaignsClient.cs
@@ -6,57 +6,57 @@ namespace Keitaro.NET.Clients;
 public interface ICampaignsClient
 {
     /// <summary>
-    /// Retrieve a list of all domains in your account
+    /// Retrieve a list of all campaigns in your account
     /// </summary>
     Task<IReadOnlyList<Campaign>> GetAll();
 
     /// <summary>
-    /// Create a new domain
+    /// Create a new campaign
     /// </summary>
     Task<Campaign> Create(Models.Requests.Campaign campaign);
     
     /// <summary>
-    /// Create a new domain
+    /// Clone a new campaign
     /// </summary>
     Task<Campaign> Clone(int campaignId, Models.Requests.Campaign campaign);
     
     /// <summary>
-    /// Retrieve a specific domain
+    /// Retrieve a specific campaign
     /// </summary>
     Task<Campaign> Get(int campaignId);
     
     /// <summary>
-    /// Update a domain
+    /// Update a campaign
     /// </summary>
     Task<Campaign> Update(int campaignId, Models.Requests.Campaign campaign);
     
     /// <summary>
-    /// Archive a domain
+    /// Archive a campaign
     /// </summary>
     Task<Campaign> Archive(int campaignId);
     
     /// <summary>
-    /// Archive a domain
+    /// Enable a campaign
     /// </summary>
     Task<Campaign> Enable(int campaignId);
     
     /// <summary>
-    /// Archive a domain
+    /// Disable a campaign
     /// </summary>
     Task<Campaign> Disable(int campaignId);
 
     /// <summary>
-    /// Update domain status
+    /// Update campaign costs
     /// </summary>
     void UpdateCosts(int campaignId, Models.Requests.Costs costs);
 
     /// <summary>
-    /// Archive a domain
+    /// Archive a campaign
     /// </summary>
     Task<Campaign> Restore(int campaignId);
 
     /// <summary>
-    /// Retrieve a list of all domains in your account
+    /// Retrieve a list of all campaigns in your account
     /// </summary>
     Task<IReadOnlyList<Campaign>> GetAllDeleted();
 }
